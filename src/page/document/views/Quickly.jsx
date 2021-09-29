8ee6e564ceb1d8766cf28f62d5d757937608a573
Suwings <Suwings@outlook.com>
2021-09-29 12:42:08 +0800

    新增 测试md

--- src/page/document/views/Quickly.jsx
+++ src/page/document/views/Quickly.jsx
@@ -3,6 +3,36 @@ import React from 'react';
 export default function Index() {
 
     return (
-        <div>让我们开始吧</div>
+        <>
+            <h3 id='这是什么'>这是什么</h3>
+            <p>MCSManager 面板（简称：MCSM 面板）是一款全中文，轻量级，开箱即用，多实例和支持 Docker 的 Minecraft 服务端管理面板。
+                在 Minecraft Java 版中已有一定的流行程度，它可以帮助你集中管理多个服务器，又或者分配账号供其他人使用而不必过于担心安全问题，对于商业活动也有一定的基础功能。</p>
+            <p>&nbsp;</p>
+            <h3 id='安装和使用'>安装和使用</h3>
+            <p>暂无</p>
+            <p>&nbsp;</p>
+            <h3 id='运行环境'>运行环境</h3>
+            <p>Linux 下运行需要 Node 14 +</p>
+            <p>Windows 一般情况下自带</p>
+            <p>&nbsp;</p>
+            <p>下载前往：<a href='https://nodejs.org/zh-cn/' target='_blank' class='url'>https://nodejs.org/zh-cn/</a></p>
+            <p>&nbsp;</p>
+            <pre><code class='language-javascript' lang='javascript'>const a = 1;
+                const b = 2;
+                function r(n) dsdasdas
+
+            </code></pre>
+            <h1 id='支持的系统'>支持的系统</h1>
+            <ul>
+                <li>Linux 系统</li>
+                <li>WIndows 64</li>
+                <li>Windows 32</li>
+
+            </ul>
+            <p>&nbsp;</p>
+            <h2 id='二号标题'>二号标题</h2>
+            <blockquote><p>你好世界，世界你好</p>
+            </blockquote>
+        </>
     )
 }
