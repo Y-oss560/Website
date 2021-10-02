20371fe420539d80d0affbb039868b7359af6223
Suwings <suwings@outlook.com>
2021-10-02 10:22:38 +0800

    新增 动态解析MD

--- src/page/document/index.jsx
+++ src/page/document/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import {
@@ -12,6 +12,7 @@ import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import ALink from '@material-ui/core/Link'
 import { Link } from "react-router-dom";
+import ReactMarkdown from 'react-markdown'
 
 import Box from '@material-ui/core/Box';
 import ListSubheader from '@material-ui/core/ListSubheader';
@@ -22,190 +23,29 @@ import QuickStart from "./views/QuickStart";
 import CreateInstanceApi from "./views/CreateInstance";
 
 import Quickly from "./views/Quickly";
+import { links } from './links';
+import $ from "jquery";
 
 export default function Index() {
 
-    const navList = [
-        {
-            title: "教程",
-            to: "",
-            type: 2
-        },
-        {
-            title: "快速开始",
-            to: "/quick_start",
-            type: 1
-        },
-        {
-            title: "搭建开发环境",
-            to: "",
-            type: 1
-        },
-        {
-            title: "身份验证",
-            to: "",
-            type: 2
-        },
-        {
-            title: "基于 API 密钥的身份验证",
-            to: "",
-            type: 1
-        },
-        {
-            title: "基于明文账号密码的身份验证",
-            to: "",
-            type: 1
-        },
-        {
-            title: "实例管理",
-            to: "",
-            type: 2
-        },
-        {
-            title: "创建实例",
-            to: "/create_instance",
-            type: 1
-        },
-        {
-            title: "修改实例",
-            to: "",
-            type: 1
-        },
-        {
-            title: "删除实例",
-            to: "",
-            type: 1
-        },
-        {
-            title: "获取实例列表",
-            to: "",
-            type: 1
-        },
-        {
-            title: "与实例建立 Websocket 连接",
-            to: "",
-            type: 1
-        },
-        {
-            title: "开启实例",
-            to: "",
-            type: 1
-        },
-        {
-            title: "关闭实例",
-            to: "",
-            type: 1
-        },
-        {
-            title: "重启实例",
-            to: "",
-            type: 1
-        },
-        {
-            title: "终止实例",
-            to: "",
-            type: 1
-        },
-        {
-            title: "远程服务管理",
-            to: "",
-            type: 2
-        },
-        {
-            title: "创建连接",
-            to: "",
-            type: 1
-        },
-        {
-            title: "删除连接",
-            to: "",
-            type: 1
-        },
-        {
-            title: "立刻连接",
-            to: "",
-            type: 1
-        },
-        {
-            title: "更新密钥",
-            to: "",
-            type: 1
-        },
-        {
-            title: "获取所有远程服务信息",
-            to: "",
-            type: 1
-        },
-        {
-            title: "根据条件查询远程服务",
-            to: "",
-            type: 1
-        },
-        {
-            title: "计划任务管理",
-            to: "",
-            type: 2
-        },
-        {
-            title: "创建计划任务",
-            to: "",
-            type: 1
-        },
-        {
-            title: "修改计划任务",
-            to: "",
-            type: 1
-        },
-        {
-            title: "删除计划任务",
-            to: "",
-            type: 1
-        },
-        {
-            title: "获取计划任务列表",
-            to: "",
-            type: 1
-        },
-        {
-            title: "镜像管理",
-            to: "",
-            type: 2
-        },
-        {
-            title: "创建镜像",
-            to: "",
-            type: 1
-        },
-        {
-            title: "获取镜像列表",
-            to: "",
-            type: 1
-        },
-        {
-            title: "删除镜像",
-            to: "",
-            type: 1
-        },
-        {
-            title: "获取镜像列表",
-            to: "",
-            type: 1
-        },
-        {
-            title: "获取容器列表",
-            to: "",
-            type: 1
-        },
-        {
-            title: "获取镜像构建进度",
-            to: "",
-            type: 1
-        },
-    ]
+    const navList = links()
 
     let { path, url } = useRouteMatch();
+    const [markdownText, setMarkdownText] = useState("www");
 
-    console.log(path, url)
+    async function onSelectApi(to) {
+        const url = `/markdown${to}.md`;
+        console.log(`Req:${url}`)
+        const response = $.ajax({
+            url,
+            success() {
+                console.log(response.responseText)
+                const text = response.responseText;
+                setMarkdownText(text);
+            }
+        });
+
+    }
 
     return (
         <Container maxWidth="lg">
@@ -224,40 +64,15 @@ export default function Index() {
                                     }
                                     return (
                                         <li style={{ margin: "4px 0px", fontSize: "13px" }}>
-                                            <Link to={url + element.to}>
-                                                <ALink>{element.title} | {url + element.to}</ALink>
-                                            </Link>
+                                            <ALink onClick={() => onSelectApi(element.to)}>{element.title} | {element.to}</ALink>
                                         </li>
                                     )
                                 })
                             }
                         </ul>
-                        {/*<Box sx={{ width: '100%', maxWidth: 360 }}>
-                            <nav aria-label="">
-                                <List>
-                                    {
-                                        navList.map((element) => {
-                                        if (element.type === 2) {
-                                            return (
-                                                <li>
-                                                    <p>{element.title}</p>
-                                                </li>
-                                            )
-                                        }
-                                        return (
-                                            <span>
-                                                <ListItemText to={url + element.to} primary="{element.title} | {url + element.to}" />
-                                            </span>
-                                                
-                                        )
-                                    })
-                                    }
-                                </List>
-                            </nav>
-                        </Box>*/}
                     </Grid>
                     <Grid item xs={8}>
-                        <Switch>
+                        {/* <Switch>
                             <Route path={`${path}/quick_start`}>
                                 <QuickStart />
                             </Route>
@@ -267,9 +82,10 @@ export default function Index() {
                             <Route path={`${path}`}>
                                 <Quickly />
                             </Route>
-                        </Switch>
+                        </Switch> */}
+                        <ReactMarkdown children={markdownText} />
                     </Grid>
-                    
+
                 </Grid>
             </div>
         </Container>
---
+++ src/page/document/links.jsx
@@ -0,0 +1,179 @@
+export function links() {
+    return [
+        {
+            title: "教程",
+            to: "",
+            type: 2
+        },
+        {
+            title: "快速开始",
+            to: "/quick_start",
+            type: 1
+        },
+        {
+            title: "搭建开发环境",
+            to: "",
+            type: 1
+        },
+        {
+            title: "身份验证",
+            to: "",
+            type: 2
+        },
+        {
+            title: "基于 API 密钥的身份验证",
+            to: "",
+            type: 1
+        },
+        {
+            title: "基于明文账号密码的身份验证",
+            to: "",
+            type: 1
+        },
+        {
+            title: "实例管理",
+            to: "",
+            type: 2
+        },
+        {
+            title: "创建实例",
+            to: "/create_instance",
+            type: 1
+        },
+        {
+            title: "修改实例",
+            to: "",
+            type: 1
+        },
+        {
+            title: "删除实例",
+            to: "",
+            type: 1
+        },
+        {
+            title: "获取实例列表",
+            to: "",
+            type: 1
+        },
+        {
+            title: "与实例建立 Websocket 连接",
+            to: "",
+            type: 1
+        },
+        {
+            title: "开启实例",
+            to: "",
+            type: 1
+        },
+        {
+            title: "关闭实例",
+            to: "",
+            type: 1
+        },
+        {
+            title: "重启实例",
+            to: "",
+            type: 1
+        },
+        {
+            title: "终止实例",
+            to: "",
+            type: 1
+        },
+        {
+            title: "远程服务管理",
+            to: "",
+            type: 2
+        },
+        {
+            title: "创建连接",
+            to: "",
+            type: 1
+        },
+        {
+            title: "删除连接",
+            to: "",
+            type: 1
+        },
+        {
+            title: "立刻连接",
+            to: "",
+            type: 1
+        },
+        {
+            title: "更新密钥",
+            to: "",
+            type: 1
+        },
+        {
+            title: "获取所有远程服务信息",
+            to: "",
+            type: 1
+        },
+        {
+            title: "根据条件查询远程服务",
+            to: "",
+            type: 1
+        },
+        {
+            title: "计划任务管理",
+            to: "",
+            type: 2
+        },
+        {
+            title: "创建计划任务",
+            to: "",
+            type: 1
+        },
+        {
+            title: "修改计划任务",
+            to: "",
+            type: 1
+        },
+        {
+            title: "删除计划任务",
+            to: "",
+            type: 1
+        },
+        {
+            title: "获取计划任务列表",
+            to: "",
+            type: 1
+        },
+        {
+            title: "镜像管理",
+            to: "",
+            type: 2
+        },
+        {
+            title: "创建镜像",
+            to: "",
+            type: 1
+        },
+        {
+            title: "获取镜像列表",
+            to: "",
+            type: 1
+        },
+        {
+            title: "删除镜像",
+            to: "",
+            type: 1
+        },
+        {
+            title: "获取镜像列表",
+            to: "",
+            type: 1
+        },
+        {
+            title: "获取容器列表",
+            to: "",
+            type: 1
+        },
+        {
+            title: "获取镜像构建进度",
+            to: "",
+            type: 1
+        },
+    ]
+}
