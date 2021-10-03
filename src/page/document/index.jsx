2cf9b08ffd3644200e0b005db0f305f8e3d8e1a6
Suwings <suwings@outlook.com>
2021-10-03 19:42:20 +0800

    新增 依赖库&未编写的页面提示

--- src/page/document/index.jsx
+++ src/page/document/index.jsx
@@ -7,18 +7,37 @@ import $ from "jquery";
 
 import { links } from './links';
 import "./md.css"
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function Index() {
 
     // Markdown 文本变量
     const [markdownText, setMarkdownText] = useState("开发版本，请选择左侧菜单显示远程动态内容");
-
+    // Other State
+    const [openAlert, setOpenAlert] = React.useState({
+        open: false,
+        title: ""
+    });
     // 获取菜单项
     const navList = links();
 
     // 选择 API 事件，自动远程加载
     async function onSelectApi(to) {
+        if (!to) {
+            if (!openAlert.open) {
+                setOpenAlert({
+                    open: true,
+                    title: "此页面还在编写中，过几天再来看看吧"
+                });
+            }
+            return
+        }
         let url = `/markdown${to}.md`;
         if (to === "/" || !to) {
             url = `/markdown/quick_start.md`;
@@ -34,37 +53,52 @@ export default function Index() {
 
     }
 
+    function closeAlert() {
+        setOpenAlert({
+            open: false,
+            title: ""
+        })
+    }
+
     // HTML 模板
     return (
-        <Container maxWidth="lg">
-            <div style={{ marginTop: "24px" }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        <ul style={{ listStyleType: " none" }}>
-                            {
-                                navList.map((element) => {
-                                    if (element.type === 2) {
-                                        return (
-                                            <li>
-                                                <h3>{element.title}</h3>
-                                            </li>
-                                        )
-                                    } else {
-                                        return (
-                                            <li style={{ margin: "2px 0px", fontSize: "14px" }}>
-                                                <ALink style={{ cursor: "pointer" }} onClick={() => onSelectApi(element.to)}>{element.title}</ALink>
-                                            </li>
-                                        )
-                                    }
-                                })
-                            }
-                        </ul>
+        <>
+            <Container maxWidth="lg">
+                <div style={{ marginTop: "24px" }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                            <ul style={{ listStyleType: " none" }}>
+                                {
+                                    navList.map((element) => {
+                                        if (element.type === 2) {
+                                            return (
+                                                <li>
+                                                    <h3>{element.title}</h3>
+                                                </li>
+                                            )
+                                        } else {
+                                            return (
+                                                <li style={{ margin: "2px 0px", fontSize: "14px" }}>
+                                                    <ALink style={{ cursor: "pointer" }} onClick={() => onSelectApi(element.to)}>{element.title}</ALink>
+                                                </li>
+                                            )
+                                        }
+                                    })
+                                }
+                            </ul>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <ReactMarkdown children={markdownText} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={8}>
-                        <ReactMarkdown children={markdownText} />
-                    </Grid>
-                </Grid>
-            </div>
-        </Container>
+                </div>
+            </Container>
+
+            <Snackbar open={openAlert.open} anchorOrigin={{ vertical: "top", horizontal: "center" }} autoHideDuration={3000} onClose={closeAlert}>
+                <Alert severity="warning" onClose={closeAlert}>
+                    {openAlert.title}
+                </Alert>
+            </Snackbar>
+        </>
     )
 }
