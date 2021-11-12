00e5e18f7fff3af738386155246971e1c3efe4f6
Suwings <Suwings@outlook.com>
2021-11-12 15:06:57 +0800

    新增 第一阶段的演示

---
+++ components/Reception.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import style from "./css/reception.module.css";
+
+export default function Reception() {
+  return (
+    <>
+      <div>
+        <div className={style.index}>
+          <div
+            st
+            className={style.ItemCrad}
+            style={{ top: "-10px", left: "80px" }}
+          ></div>
+          <div
+            st
+            className={style.ItemCrad}
+            style={{ top: "-20px", left: "350px" }}
+          ></div>
+          <div
+            st
+            className={style.ItemCrad}
+            style={{ top: "-30px", left: "700px" }}
+          ></div>
+          <div
+            st
+            className={style.ItemCrad}
+            style={{ top: "-40px", left: "80px" }}
+          ></div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import Reception from "../components/Reception";
 import AppInfo from "../components/AppInfo";
 export default function index() {
   return (
     <>
       <Header></Header>
       <main>
-        <Main></Main>
+        <Reception></Reception>
         <AppInfo></AppInfo>
       </main>
       <Footer></Footer>
