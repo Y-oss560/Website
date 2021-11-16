fc8b2ad9f3e67cacd4b5305f1b5fa694f4fb45ef
Suwings <Suwings@outlook.com>
2021-11-16 14:33:25 +0800

    修改 最小高度

--- components/Reception.jsx
+++ components/Reception.jsx
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from "react";
-import Container from "@material-ui/core/Container";
 import style from "./css/reception.module.css";
-import { Hidden, Button } from "@material-ui/core";
-import Grid from '@material-ui/core/Grid';
+import { Button } from "@material-ui/core";
 
 export default function Reception() {
-
-  const [height, setHeight] = useState(400)
+  const [height, setHeight] = useState(400);
 
   useEffect(() => {
     // window is accessible here.
     console.log("window.innerHeight", window.innerHeight);
-    setHeight(window.innerHeight)
+    setHeight(window.innerHeight);
   }, []);
-
 
   return (
     <>
       <div>
-        <div className={style.Index} style={{ height, minHeight: "800px" }}>
+        <div className={style.Index} style={{ height, minHeight: "600px" }}>
           <div className={style.TextHello}>
             <h1>
               <span className={style.Highlight}>分布式</span>，
@@ -28,12 +24,26 @@ export default function Reception() {
               <span className={style.Highlight}> Minecraft </span>
               <span>和其他游戏的服务端管理面板</span>
             </h1>
-            <p className="sub-title" style={{ fontSize: "14px" }}>现在，您可免费获取它并开始使用！</p>
+            <p className="sub-title" style={{ fontSize: "14px" }}>
+              现在，您可免费获取它并开始使用！
+            </p>
             <div className={style.TextButtonPanel}>
-              <Button variant="contained" color="primary" size="large" className={`${style.TextButtonQuickStart} ${style.TextButton}`} style={{ margin: "10px", backgroundColor: "#1a7808" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={`${style.TextButtonQuickStart} ${style.TextButton}`}
+                style={{ margin: "10px", backgroundColor: "#1a7808" }}
+              >
                 开始使用
               </Button>
-              <Button variant="contained" color="primary" size="large" className={`${style.TextButton}`} style={{ margin: "10px" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={`${style.TextButton}`}
+                style={{ margin: "10px" }}
+              >
                 了解更多
               </Button>
             </div>
